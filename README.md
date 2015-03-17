eedb1d93e12f113fdf71e452154b89fb82798dad
Ravikiran Janardhana <rjanardhana@tripadvisor.com>
2015-03-17 16:14:41 -0700

    First commit of the cleaned dish list file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# restaurant-menu-items
-http://menus.nypl.org/data powered restaurant menu items
+restaurant-menu-items
+=======================
+* http://menus.nypl.org/data powered restaurant menu items
+* I would like to sincerely thank [New York Public Library](http://www.nypl.org/) for putting the menu dataset out in the public. Please credit them as your source in your applications or publications if you reuse this repository data
+
+Setup
+=====
+Download NYPL data
+------------------
+* Visit http://menus.nypl.org/data and grab the latest compressed menu file
+* Extract the files and put in the `data` folder
+* Run `gen_menu_items.py` to dump the cleaned up list
---
+++ src/gen_menu_items.py
@@ -0,0 +1,137 @@
+__author__ = 'rjanardhana'
+
+import csv
+import sys
+import logging
+import os
+import string
+import time
+import re
+
+# Global logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Global
+DISH_CSV_FILE = "../data/Dish.csv"
+CLEANED_DISH_LIST_FILE = "../data/CleanedDishList.csv"
+ALPHA_WITH_SPACE_REGEX = re.compile("^[a-z\s]+$")
+MIN_DISH_LEN = 3
+MAX_WORDS_IN_DISH_LEN = 6
+
+
+def pre_process(word):
+    """
+    Preprocesses the word and returns a cleaned up version by converting it to lower case and removes punctuation
+    :param word: input word to preprocess
+    :returns: cleaned up version of the input word
+    """
+    if not word:
+        return ""
+
+    # Lower case
+    s = word.lower()
+
+    # Get rid of apostrophe
+    s = s.replace("'",  "")
+
+    # Replace other punctuations with a space
+    for char in string.punctuation:
+        s = s.replace(char, " ")
+
+    # Replace multiple spaces with a single space
+    s = re.sub('\s+', ' ', s)
+
+    # Strip spaces at the end
+    s = s.strip()
+
+    return s
+
+
+def is_valid(dish):
+    """
+    :returns: true if the dish matches all the rules, else false
+    """
+    # min length of dish
+    if len(dish) < MIN_DISH_LEN:
+        return False
+
+    # has only alphabets and space
+    if not ALPHA_WITH_SPACE_REGEX.search(dish):
+        return False
+
+    word_list = dish.split(" ")
+
+    # max words in dish check
+    if len(word_list) > MAX_WORDS_IN_DISH_LEN:
+        return False
+
+    # has at least one word within that satisfies min_len check
+    has_word_with_min_len = False
+    for word in word_list:
+        if len(word) > MIN_DISH_LEN:
+            has_word_with_min_len = True
+            break
+    if not has_word_with_min_len:
+        return False
+
+    # cannot have more than 1 single letter word
+    single_letter_word_count = 0
+    for word in word_list:
+        if len(word) == 1:
+            single_letter_word_count += 1
+            if single_letter_word_count > 1:
+                return False
+
+    # passed every check
+    return True
+
+
+def generate_menu_items():
+    """
+    Reads dishes from DISH_CSV_FILE and writes the cleaned up dish version to CLEANED_DISH_LIST_FILE
+    :return:
+    """
+    if not os.path.exists(DISH_CSV_FILE):
+        logger.error("%s does not exist, cannot proceed", DISH_CSV_FILE)
+        sys.exit(1)
+
+    logger.info("Started processing file")
+    t0 = time.clock()
+    ip = open(DISH_CSV_FILE, "rb")
+    csv_reader = csv.reader(ip, quoting=csv.QUOTE_MINIMAL)
+
+    op = open(CLEANED_DISH_LIST_FILE, "wb")
+    csv_writer = csv.writer(op, quoting=csv.QUOTE_MINIMAL)
+
+    # skip header
+    next(csv_reader, None)
+
+    cleaned_dishes = set()
+    count = 0
+    for row in csv_reader:
+        raw_dish_list = row[1].split(",")
+        for raw_dish in raw_dish_list:
+            preprocessed_dish = pre_process(raw_dish)
+            if is_valid(preprocessed_dish):
+                cleaned_dishes.add(preprocessed_dish)
+        count += 1
+        if count % 50000 == 0:
+            logger.info("Processed %d lines", count)
+
+    cleaned_dishes = sorted(cleaned_dishes)
+    logger.info("Dish count = %d", len(cleaned_dishes))
+
+    ip.close()
+    t1 = time.clock()
+    logger.info("Finished processing in %f seconds", t1-t0)
+
+    logger.info("Writing cleaned up menu items to %s", CLEANED_DISH_LIST_FILE)
+    t2 = time.clock()
+    for dish in cleaned_dishes:
+        csv_writer.writerow([dish])
+    t3 = time.clock()
+    logger.info("Finished writing to disk in %f seconds", t3-t2)
+
+if __name__ == "__main__":
+    generate_menu_items()
